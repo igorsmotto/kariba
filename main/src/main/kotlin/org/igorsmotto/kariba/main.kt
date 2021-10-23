1dc2da18518c696aa115c69b3a66536d660eee85
igorsmotto <igor.lema@ifood.com.br>
2021-10-23 14:22:09 -0300

    Setup all dependencies of CleanArch and create distribution file to run the CLI

--- main/src/main/kotlin/org/igorsmotto/kariba/main.kt
+++
@@ -1,5 +0,0 @@
-package org.igorsmotto.kariba
-
-fun main() {
-    println("Hello World!")
-}
---
+++ src/main/kotlin/org/igorsmotto/kariba/Main.kt
@@ -0,0 +1,8 @@
+package org.igorsmotto.kariba
+
+import org.igorsmotto.kariba.cli.cli
+
+fun main(args: Array<String>) {
+    cli(args)
+}
+
